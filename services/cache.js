f8f340bed7273badd8341395eb89b117eee1e3a6
Chris Evans <mail@chr1s.it>
2020-01-21 14:23:44 +0000

    fixes for keys

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
+const keys = require('../config/keys');
 
 const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
-const keys = require('../config/keys');
 
 mongoose.Query.prototype.cache = function(options = {}) {
   this.useCache = true;
